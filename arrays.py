f99e5e159a7ac2193b1d7e94982993bba34956a6
Jim Bluemel <theshao@gmail.com>
2018-02-19 22:40:13 +0000

    Add gamma correction and extra programs Gamma correction via lookup table in lighttools.py Start of program using colours reuqested by Sarah

--- arrays.py
+++ arrays.py
@@ -12,7 +12,6 @@ LED_FREQ_HZ     = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA         = 10      # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS  = 255      # Set to 0 for darkest and 255 for brightest
 LED_INVERT      = False   # True to invert the signal (when using NPN transistor level shift)
-
 # INSTRUMENT
 TUBE_COUNT      = 8       # Number of strips
 TUBE_LENGTH     = 1170  # mm
@@ -35,7 +34,7 @@ lidars.init(DEVSTRINGS)
 
 # LED strip setup
 print("Seting up LED strip with %d LEDs" %PIXEL_COUNT)
-strip = neopixel.Adafruit_NeoPixel(PIXEL_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip = neopixel.Adafruit_NeoPixel(PIXEL_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL))
 strip.begin()
 
 def updatepixels(colours):
--- programs.py
+++ programs.py
@@ -112,3 +112,66 @@ class prog_Fire:
                 if controller.playing:
                     controller.stop()                
         return self.current_colours
+        
+        
+class prog_Sarah:
+    COLOURS = [[114, 238, 88], [151, 250, 119], [197, 250, 143], [223, 255, 210], [189, 217, 214], [195, 210, 254], [95, 132, 198], [67, 89, 168]]
+    mysynths = []
+    # Sound properties
+    def __init__(self, tube_count, led_per_tube):
+        self.tube_length = led_per_tube
+        self.tube_count = tube_count
+        ROOT_FREQ = 220
+        SCALE = [0, 2, 3, 5, 7, 8, 11, 12]  # natural minor
+        FREQS = [ROOT_FREQ if n == 0
+                 else int(round(ROOT_FREQ * (2**(1./12))**n))  # Note is equal to (root note * (2^12)^(semitone steps from root note) )
+                 for n in SCALE]
+        # current_colours = np.tile(lighttools.BLACK, (TUBE_COUNT, LED_PER_TUBE, 1)) # col, row, rgb
+        base_colours = []
+        for tube in range(tube_count):
+            colour =[prog_Sarah.COLOURS[tube]] * led_per_tube
+            base_colours.append(colour)
+        
+        self.base_colours = np.array(base_colours) / 1.5
+        self.current_colours = self.base_colours.copy() # Initial colours match base colours
+        self.mysynths = []
+        
+        for s in range(tube_count):
+            # Add a synth for each tube at the correct freq
+            self.mysynths.append(synths.syn_Moog(FREQS[s]))
+    
+    def update(self, distances):
+        
+        r = 255
+        g = 15
+        b = 12
+        
+        for tube, distance in enumerate(distances):
+            for pixel in range(self.tube_length):
+                flicker = random.randint(0, 60)
+                r1 = r - flicker
+                g1 = g + flicker
+                b1 = max(g - flicker, 0)
+                self.current_colours[tube, pixel] = np.array([g1, r1, b1])       
+            
+            controller = self.mysynths[tube]
+            if distance > 0:           
+                # Update pixels
+                #
+                # Light up the pixel at the detected position
+                self.current_colours[tube, distance] = lighttools.WHITE #255 - base_colours[tube, pixel_dist]
+                # And a couple either side for extra brightness
+                for i in range(2):
+                        self.current_colours[tube, min(distance + i, self.tube_length - 1)] = lighttools.WHITE #255 - base_colours[tube, pixel_dist]
+                        self.current_colours[tube, max(distance - i, 0)] = lighttools.WHITE #255 - base_colours[tube, pixel_dist]
+                
+                # Update synths
+                #
+                # Set modulation and ensure sound playing
+                controller.modulate(distance)
+                if not controller.playing:
+                    controller.start()
+            else:
+                if controller.playing:
+                    controller.stop()                
+        return self.base_colours
