2771744adc2179d5d372080df4a3c69ba8e79a14
Jim Bluemel <theshao@gmail.com>
2018-04-02 22:25:45 +0000

    Various programs -colour blending initial effort, sparks

--- programs.py
+++ programs.py
@@ -225,8 +225,7 @@ class test_Rippler:
 
         return self.current_colours
 
-        
-        
+  
 class test_Fire:    
     # Loop speed as of 31/03 ~15ms
     
@@ -238,11 +237,11 @@ class test_Fire:
         self.COOLING = 50 # How quickly cells cool as they rise, 0-255
         self.SPARKING = 80 # How often new heat sparks are producted, 0-255        
         self.colour_cycle_speed = 0
-        self.midi_program = 1
+        self.midi_program = 2
         self.midi_velocity = 112
         self.midi_modulation = 1
         self.auto_play = True
-        notes = [i + 12 for i in [45, 47, 48, 50, 52, 53, 56, 57]]
+        notes = [i for i in [45, 47, 49, 50, 52, 54, 56, 57]]
         # synth(channel, program, note, velocity, modulation)
         self.synths = [synths.syn_Midi(n, self.midi_program, notes[n], self.midi_velocity, self.midi_modulation)
                         for n in range(self.tube_count)]
@@ -272,7 +271,7 @@ class test_Fire:
             
             # Drift heat upwards and diffuse with surrounding pixels
             for k in range(self.tube_length - 1, 2, -1):
-                self.heat[tube][k] = int((2 * self.heat[tube][k - 1] + 2 * self.heat[tube][k - 2] + 2 * self.heat[tube][k - 2] #) / 3)
+                self.heat[tube][k] = int((2 * self.heat[tube][k - 1] + 2 * self.heat[tube][k - 2] + 2 * self.heat[tube][k - 2]
                                             + self.heat[(tube + 1) % 8][k - 1] + self.heat[(tube - 1) % 8][k - 1]) /8)
             
             if distance > 0:                
@@ -286,7 +285,8 @@ class test_Fire:
                 # Generate a new spark at detected position
                 self.heat[tube][distance] = min(self.heat[tube][distance] + random.randint(160, 255), 255)
                 # Set all pixels below to high temperature
-                self.heat[tube][:distance] = [min(self.heat[tube][i] + random.randint(160, 255), 255) for i in range(distance)]
+                self.heat[tube][:distance] = [min(self.heat[tube][i] + random.randint(160, 255), 255)
+                                                for i in range(distance)]
                 
             else:
                 # Stop the synth
@@ -315,17 +315,17 @@ class test_Fire:
         return self.current_colours
 
         
-class test_Art:
-    COLOURS = [[112, 241, 90], [151, 250, 119], [171, 250, 130], [210, 255, 145], [205, 240, 175], [199, 224, 200], [160, 216, 189], [148, 200, 161]]
+class test_Art:    
     
     def __init__(self, tube_count, led_per_tube):
         self.tube_length = led_per_tube
         self.tube_count = tube_count
         self.midi_ratio = 127.0/led_per_tube
-        self.midi_program = 0
+        self.midi_program = 1
         self.midi_velocity = 112
         self.midi_modulation = 1
-        self.colours = [[112, 241, 90], [151, 250, 119], [171, 250, 130], [210, 255, 145], [205, 240, 175], [199, 224, 200], [160, 216, 189], [148, 200, 161]]
+        self.colours = [[112, 241, 90], [151, 250, 119], [171, 250, 130], [210, 255, 145],
+                        [205, 240, 175], [199, 224, 200], [160, 216, 189], [148, 200, 161]]
         notes = [i for i in [45, 47, 48, 50, 52, 53, 56, 57]]
         # synth(channel, program, note, velocity, modulation)
         self.synths = [synths.syn_Midi(n, self.midi_program, notes[n], self.midi_velocity, self.midi_modulation)
@@ -354,7 +354,7 @@ class test_Art:
             
             if distance > 0:
                 # Light up 5 pixels at the detected position
-                self.current_colours[tube, distance-2:distance+2] = [64, 87, 168] # lighttools.WHITE #255 - base_colours[tube, pixel_dist]
+                self.current_colours[tube, distance-2:distance+2] = [64, 87, 168]
 
                 # Update synths
                 #
@@ -368,7 +368,154 @@ class test_Art:
                 if controller.playing:
                     controller.stop()          
         return self.current_colours        
+
+class test_AAColourBlender:    
+    
+    def __init__(self, tube_count, led_per_tube):
+        self.tube_length = led_per_tube
+        self.tube_count = tube_count
+        self.midi_ratio = 127.0/led_per_tube
+        self.midi_program = 3
+        self.midi_velocity = 112
+        self.midi_modulation = 1
         
+        self.colours = [[255, 0, 0], [0, 255, 0], [0, 0, 255], [127, 127, 0],
+                        [127, 0, 127], [0, 127, 127], [80, 80, 80], [200, 200, 200]]
+
+        notes = [i + 12 for i in [45, 47, 48, 50, 52, 53, 56, 57]]
+        # synth(channel, program, note, velocity, modulation)
+        self.synths = [synths.syn_Midi(n, self.midi_program, notes[n], self.midi_velocity, self.midi_modulation)
+                        for n in range(self.tube_count)]
+        self.tick = 0
+    
+    def load(self):    
+       
+        # Set all pixels to black
+        black = np.array([0, 0, 0])               
+        self.current_colours = np.tile(black, (self.tube_count, self.tube_length, 1))
+        self.base_colours = self.current_colours.copy()
+        # Initialize a grid for each tube
+        self.grid = np.tile(0, (self.tube_count, self.tube_count, self.tube_length, 3))
+        # Load synth patch
+        for synth in self.synths:
+            synth.load_program()
+        
+    def update(self, distances):
+        self.current_colours = self.base_colours.copy()
+        self.grid[:] = 0 # np.nan #np.zeros([self.tube_count, self.tube_count, self.tube_length, 3])
+        for tube, distance in enumerate(distances):
+            controller = self.synths[tube]      
+            
+            if distance > 0:            
+                # Set colour for each pixel in this column up to distance, and in each other column at this height
+                self.grid[tube, tube, :] = self.colours[tube]
+                self.grid[tube, :, distance-2:distance+2] = self.colours[tube]
+                
+                # Update synths
+                #
+                # Set modulation and ensure sound playing
+                controller.modulate(int(min(distance * self.midi_ratio, 127)))
+                if not controller.playing:
+                    controller.start()                
+                
+            else:
+                
+                # Stop the synth
+                if controller.playing:
+                    controller.stop()          
+        
+        self.current_colours = np.clip(np.sum(self.grid, axis = 0), 0, 255).astype(np.uint8)
+        
+        return self.current_colours             
+        
+        
+class test_Abstract:    
+    
+    def __init__(self, tube_count, led_per_tube):
+        self.tube_length = led_per_tube
+        self.tube_count = tube_count
+        self.midi_ratio = 127.0/led_per_tube
+        self.midi_program = 2
+        self.midi_velocity = 112
+        self.midi_modulation = 1
+        self.colours = [[112, 241, 90], [151, 250, 119], [171, 250, 130], [210, 255, 145], [205, 240, 175], [199, 224, 200], [160, 216, 189], [148, 200, 161]]
+        self.circle_colour = np.array([255, 255, 255])
+        self.circle_spacing = 9 # Min radius of previous circle before we add another
+        notes = [i + 12 for i in [45, 47, 48, 50, 52, 53, 56, 57]]
+        # synth(channel, program, note, velocity, modulation)
+        self.synths = [synths.syn_Midi(n, self.midi_program, notes[n], self.midi_velocity, self.midi_modulation)
+                        for n in range(self.tube_count)]
+        self.tick = 0
+    
+    def load(self):
+    
+        self.base_colours = []
+        for tube in range(self.tube_count):
+            colour =[self.colours[tube]] * self.tube_length
+            self.base_colours.append(colour)
+        
+        self.base_colours = np.tile([64, 87, 168], (self.tube_count, self.tube_length, 1)) #np.array(self.base_colours)
+        self.current_colours = self.base_colours.copy() # Initial colours match base colours
+        
+        self.circles = []
+        # Load synth patch
+        for synth in self.synths:
+            synth.load_program()
+        
+    def update(self, distances):
+        self.tick += 1
+        self.current_colours = lighttools.fade(self.current_colours, self.base_colours, 5) #self.base_colours.copy()
+        
+        for tube, distance in enumerate(distances):
+            controller = self.synths[tube]      
+            
+            if distance > 0:            
+                #Check whether we've already got circles on this tube
+                #position = [tube, distance]
+                col_matches = [circle for circle in self.circles if tube == circle[0]] # and circle[2] < 9]
+                space_matches = []
+                # Move any centred around the wrong position
+                for circle in col_matches:
+                    circle[1] = distance
+                    # Check if this circle prevents us adding a new one
+                    if circle[2] < self.circle_spacing:
+                        space_matches.append(circle)
+                if len(space_matches) < 1:                    
+                    # Add a circle centered on detected position
+                    self.circles.append([tube, distance, 0]) # x, y, radius
+                # Update synths
+                #
+                # Set modulation and ensure sound playing
+                controller.modulate(int(min(distance * self.midi_ratio, 127)))
+                if not controller.playing:
+                    controller.start()                
+                
+            else:
+                # Remove all circles from this tube
+                self.circles = [circle for circle in self.circles if not tube == circle[0]]
+                # Stop the synth
+                if controller.playing:
+                    controller.stop()          
+        for circle in self.circles:
+            x, y, radius = circle
+            #Fade the circle as it expands
+            colour = self.circle_colour #* 1/radius if radius > 0 else self.circle_colour
+            # Draw the circle
+            self.current_colours[x, min(y + radius, self.tube_length - 1)] = colour
+            self.current_colours[x, max(y - radius, 0)] = colour
+            self.current_colours[min(x + radius/5, self.tube_count - 1), y] = colour
+            self.current_colours[max(x - radius/5, 0), y] = colour
+            #np.clip(self.current_colours, 0, 255, out=self.current_colours)
+            if self.tick % 2 == 0:
+                # Expand the circle
+                radius += 1
+                # Remove expired circles
+                if radius > self.tube_length:
+                    self.circles.remove(circle)
+                else:
+                    circle[2] = radius
+        #print(self.circles)        
+        return self.current_colours        
         
 class template:
     
--- synths.py
+++ synths.py
@@ -1,6 +1,6 @@
 import sc, time
 from rtmidi.midiutil import open_midioutput
-from rtmidi.midiconstants import NOTE_OFF, NOTE_ON, CONTROL_CHANGE, PROGRAM_CHANGE
+from rtmidi.midiconstants import NOTE_OFF, NOTE_ON, CONTROL_CHANGE, PROGRAM_CHANGE, ALL_NOTES_OFF
 
 print("Setting up midi")
 try:
@@ -8,136 +8,6 @@ try:
     midiout, port_name = open_midioutput(2, 1)
 except (EOFError, KeyboardInterrupt):
     sys.exit()
- 
-class syn_Moog():    
-    # moogbass synthdef from Steal this Sound
-    # arg out = 0, pan = 0, freq = 440, amp = 0.1, gate = 1, cutoff = 1000, gain = 2.0,
-    # lagamount = 0.01, att = 0.001, dec = 0.3, sus = 0.9, rel = 0.2, chorus = 0.7;
-    
-    min = 1000 # min cutoff
-    
-    def __init__(self, freq):
-        path = "moogbass"
-        synth = sc.Synth(path, 2000 + freq)
-        time.sleep(0.1)
-        synth.cutoff = self.min # LPFilter freq
-        synth.gain = 3 # Filter gain
-        synth.amp = 0.5#
-        synth.rel = 2 #
-        #synth.att = 2
-        synth.chorus = 0
-        synth.gate = 0
-        synth.freq = freq    
-        self.synth = synth        
-        self.playing = False
-    
-    def modulate(self, value): # Input range 0 - 1000
-        self.synth.cutoff = value * 40 + self.min
-        self.synth.chorus = value/24.
-        
-    def start(self):
-        self.synth.run()
-        self.synth.gate = 1
-        self.playing = True
-        
-    def stop(self):
-        self.synth.gate = 0
-        self.playing = False
-
-class syn_Lead():    
-    
-    min = 200 # min cutoff
-    
-    def __init__(self, freq):
-        path = "cs80lead_mh"
-        synth = sc.Synth(path, 6000 + freq, args=["freq", freq])
-        time.sleep(0.1)
-        synth.cutoff = self.min # LPFilter freq
-        synth.gate = 0
-        synth.sus = 2
-        synth.fsus = 2
-        synth.vibrate = 10
-        synth.freq = freq    
-        self.synth = synth        
-        self.playing = False
-    
-    def modulate(self, value): # Input range 0 - 1000
-        self.synth.cutoff = int(value / 5) + self.min
-        self.synth.vibrate = value/100
-        
-    def start(self):
-        self.synth.run()
-        self.synth.gate = 1
-        self.playing = True
-        
-    def stop(self):
-        self.synth.gate = 0
-        self.playing = False
-
-class syn_AM():    
-    # moogbass synthdef from Steal this Sound
-    # arg out = 0, pan = 0, freq = 440, amp = 0.1, gate = 1, cutoff = 1000, gain = 2.0,
-    # lagamount = 0.01, att = 0.001, dec = 0.3, sus = 0.9, rel = 0.2, chorus = 0.7;
-    
-    min = 1000 # min cutoff
-    
-    def __init__(self, freq):
-        path = "AM"
-        synth = sc.Synth(path, 4000 + freq)
-        time.sleep(0.1)
-        synth.modfreq = 1
-        synth.amp = 0.5#
-        synth.rel = 2 #
-        #synth.att = 2
-        #synth.chorus = 0
-        synth.gate = 0        
-        synth.freq = freq
-        self.synth = synth        
-        self.playing = False
-    
-    def modulate(self, value): # Input range 0 - 1000
-        self.synth.modfreq = value * 2        
-        
-    def start(self):
-        self.synth.run()
-        self.synth.gate = 1
-        self.playing = True
-        
-    def stop(self):
-        self.synth.gate = 0
-        self.playing = False
-        
-class syn_Saw():    
-    # arg freq=440, amp=0.3, fat=0.0033, ffreq=2000, atk=0.001, dec=0.3, sus=0.5, rls=0.1,gate=1;
-    
-    min = 500 # min cutoff
-    
-    def __init__(self, freq):
-        path = "fatsaw"
-        synth = sc.Synth(path, 1000 + freq)    
-        synth.ffreq = self.min # LPFilter freq
-        synth.fat = 0.5 # Filter gain
-        synth.sus = 0.9
-        synth.rls = 2
-        synth.amp = 0.3        
-        synth.gate = 0        
-        synth.freq = freq    
-        self.synth = synth        
-        self.playing = False
-    
-    def modulate(self, value): # Input range 0 - 1000
-        self.synth.ffreq = value * 2 + self.min
-        self.synth.fat = value/1000.
-        
-    def start(self):
-        self.synth.gate = 1
-        self.playing = True
-        
-    def stop(self):
-        self.synth.gate = 0
-        self.playing = False
-       
-class syn_Organdonor():    
     # arg out = 0, pan = 0.0, freq = 440, amp = 0.1, gate = 1,
     # att = 0.01, dec = 0.5, sus = 1, rel = 0.5
     # lforate = 10, lfowidth = 0.01, cutoff = 100, rq = 0.5;
@@ -189,7 +59,8 @@ class syn_Midi():
     
     def load_program(self):
         self.playing = False
-        midiout.send_message([PROGRAM_CHANGE + self.channel, self.midi_program])
+        midiout.send_message([ALL_NOTES_OFF + self.channel, 0])
+        midiout.send_message([PROGRAM_CHANGE + self.channel, self.midi_program])        
         print([PROGRAM_CHANGE + self.channel, self.midi_program])
     
     def modulate(self, value): # Input range 0 - 127
